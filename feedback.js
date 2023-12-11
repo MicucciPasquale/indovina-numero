eb61ec6b2bae2f49ad6428c34aee4b98f1f0be8a
PasqualeMdev <pasqualeegiorgia07062013@gmail.com>
2023-12-11 17:49:28 +0100

    modifiche relative all invio nella pagina html su js

--- feedback.js
+++ feedback.js
@@ -6,7 +6,11 @@ function submitForm() {
 }
 
 
-
+/*
 document.getElementById("return-first-page").addEventListener("click", function() {
     window.location.href = "file:///C:/Users/Utente/Desktop/DIGITAZON%20-%20corso/indovina%20numero/indovina-numero/index.html";
   });
+  */
+  document.getElementById("return-first-page").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
